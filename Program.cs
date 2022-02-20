b3ea5fd240a544e86356dd6929d4fad289e5474e
raubenw <raubenw@gmail.com>
2022-02-20 21:43:10 +0200

    Hierdie is my eerste commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharpAnonymousDelegates
+{
+
+    public delegate string MyDelegate(int arg1, int arg2);
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            MyDelegate f = delegate (int a, int b)
+            {
+                return (a + b).ToString();
+            };
+
+            Console.WriteLine("Anonymous delegate result = " + f(20, 30));
+        }
+    }
+}
